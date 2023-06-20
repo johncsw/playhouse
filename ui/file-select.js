267efbd5762b2ac03e56808b71dc78656780a3dd
johncsw <jobcsw1012@gmail.com>
2023-06-20 17:26:59 +0800

    ui: update page after selecting a video
    Addresses: #2

---
+++ ui/file-select.js
@@ -0,0 +1,28 @@
+const fileInput = document.getElementById('fileInput');
+const fileInfoText = document.getElementById('fileInfo');
+const uploadButton = document.getElementById('uploadButton');
+
+fileInput.addEventListener('change', function() {
+    const selectedFile = fileInput.files[0];
+
+    if (selectedFile) {
+        const fileName = selectedFile.name;
+        const fileSize = formatFileSize(selectedFile.size);
+        const fileType = selectedFile.type || 'Unknown';
+
+        fileInfoText.textContent = `Name: ${fileName}\nSize: ${fileSize}\nType: ${fileType}`;
+        uploadButton.disabled = false;
+    } else {
+        uploadButton.disabled = true; // Disable the upload button
+    }
+});
+
+function formatFileSize(size) {
+    if (size === 0) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(size) / Math.log(k));
+
+    return parseFloat((size / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
