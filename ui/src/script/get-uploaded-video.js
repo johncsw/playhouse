682230e9076df277b72d71aa569de31ee767a374
johncsw <jobcsw1012@gmail.com>
2023-07-26 19:21:46 +0800

    ui: get all uploaded videos by current session on the upload home page
    Also:
    - remove unused files
    
    Addresses: #41

---
+++ ui/src/script/get-uploaded-video.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+
+    const path = `${config.API_URL}/video/all`
+    const videoList = document.getElementById('uploadedVideoList');
+    videoList.innerHTML = ''
+
+    fetch(path, { headers : {
+            'Authorization': config.SESSION_TOKEN
+        }})
+        .then(response => {
+            if (!response.ok) {
+                const li = document.createElement('li');
+                li.textContent = 'Error loading video';
+                li.style.color = 'red';
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length === 0) {
+                const li = document.createElement('li');
+                li.textContent = 'No video uploaded';
+                li.style.color = 'yellow';
+                videoList.appendChild(li);
+            }
+
+            for (let i = 0; i < data.length; i++) {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.textContent = data[i].name;
+                a.href = data[i].link;
+
+                li.appendChild(a);
+                videoList.appendChild(li);
+            }
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
