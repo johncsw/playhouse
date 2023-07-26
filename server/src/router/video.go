99baf352f137297195c9184a7728eb090668f7ad
johncsw <jobcsw1012@gmail.com>
2023-07-26 17:22:04 +0800

    server: create endpoints retrieving streaming content
    Addresses: #39

--- server/src/router/video.go
+++ server/src/router/video.go
@@ -17,12 +17,13 @@ func newVideoRouter() *chi.Mux {
 	r.Use(middleware.AuthHandler)
 	repoFact := repository.NewFactory()
 	r.Group(func(r chi.Router) {
-		r.Get("/streaming/{videoID}", VideoStreamingHandler(repoFact))
+		r.Get("/streaming/{videoID}", GetManifestHandler(repoFact))
+		r.Get("/streaming/{videoID}/{m4sFileName}", GetStreamingContentHanlder(repoFact))
 	})
 	return r
 }
 
-func VideoStreamingHandler(repoFact *repository.Factory) http.HandlerFunc {
+func GetManifestHandler(repoFact *repository.Factory) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		videoID, convErr := strconv.Atoi(chi.URLParam(r, "videoID"))
 		if convErr != nil {
@@ -57,5 +58,45 @@ func VideoStreamingHandler(repoFact *repository.Factory) http.HandlerFunc {
 
 		manifestPath := fmt.Sprintf("%s/%d-out.mpd", URLToStream, videoID)
 		http.ServeFile(w, r, manifestPath)
+
+	}
+}
+
+func GetStreamingContentHanlder(repoFact *repository.Factory) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		videoID, convErr := strconv.Atoi(chi.URLParam(r, "videoID"))
+		if convErr != nil {
+			panic(responsebody.ResponseErr{
+				Code:    http.StatusBadRequest,
+				ErrBody: convErr,
+			})
+		}
+
+		videoRepo := repoFact.NewVideoRepo()
+		URLToStream, isTransCoded, videoErr := videoRepo.IsVideoAvailableToStream(videoID)
+		if videoErr != nil {
+			panic(responsebody.ResponseErr{
+				Code:    http.StatusBadRequest,
+				ErrBody: videoErr,
+			})
+		}
+
+		if URLToStream == "" {
+			panic(responsebody.ResponseErr{
+				Code:    http.StatusNotFound,
+				ErrBody: errors.New("video not found"),
+			})
+		}
+
+		if !isTransCoded {
+			panic(responsebody.ResponseErr{
+				Code:    http.StatusServiceUnavailable,
+				ErrBody: errors.New("transcoding to the video is not finished yet"),
+			})
+		}
+
+		m4sFileName := chi.URLParam(r, "m4sFileName")
+		m4sFilePath := fmt.Sprintf("%s/%s", URLToStream, m4sFileName)
+		http.ServeFile(w, r, m4sFilePath)
 	}
 }
